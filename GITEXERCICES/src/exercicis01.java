4fcc0768b5ca989c715a2e6b20d42093c2ff4869
JoanLikes <JoanLikes@192.168.7.154>
2015-01-16 16:12:34 +0100

    Commit inicial

---
+++ GITEXERCICES/src/exercicis01.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author JoanLikes
+ *
+ */
+public class exercicis01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
